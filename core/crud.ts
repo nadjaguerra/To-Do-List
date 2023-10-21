c105642de5f2300c8063cf4672cb653a788454e7
nadjaguerra <nadjaguerra77@gmail.com>
2023-10-21 15:15:06 -0300

    :closed_book:

--- core/crud.ts
+++ core/crud.ts
@@ -2,13 +2,24 @@ import fs from "fs"; // ES6
 
 //const fs = require ('fs')       // lidar com operações de sistemas de arquivo
 const DB_FILE_PATH = "./core/db"
-console.log("[CRUD]")
+console.log("[CRUD]");
 
 function create(content: string){
-    // salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content)                        //escrever dados em um arquivo de forma síncrona
-    return content
+    //objeto
+    const todo = {
+        
+        content: content,
+        };
+        console.log(todo);
+    
+    // salvar o content no sistema | o stringify converte um objeto em string
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify(todo));                       //escrever dados em um arquivo de forma síncrona
+    return content;
 }
-
+function read(){
+    const db = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    return db;
+}
 //simulação
-console.log(create("Hello, Hello!!! "))
+create("first ToDo ");
+console.log(read());
