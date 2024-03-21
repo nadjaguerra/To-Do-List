b69a3c69ef1362d5e44c31eec5df522136975e75
nadjaguerra <nadjaguerra77@gmail.com>
2024-03-21 15:56:55 -0300

    crud

--- core/crud.ts
+++ core/crud.ts
@@ -5,24 +5,24 @@ import { v4 as uuid } from "uuid";
 const DB_FILE_PATH = "./core/db";
 console.log("[CRUD]");
 
+type UUID = string;
+
 interface Todo {
-  id: string;
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
 }
 
 function create(content: string): Todo {
-  
   //const fullUUID = uuid();
   // Pega apenas os primeiros 8 caracteres do UUID
   //const shortUUID = fullUUID.substring(0, 8);
 
-
   const todo: Todo = {
-   // id: shortUUID, // Usa apenas os primeiros 8 caracteres do UUID
+    // id: shortUUID, // Usa apenas os primeiros 8 caracteres do UUID
 
-    id: uuid (), // a lib UUID cria um identificador unico universal
+    id: uuid(), // a lib UUID cria um identificador unico universal
     date: new Date().toLocaleString(),
     content: content,
     done: false,
@@ -53,16 +53,19 @@ function create(content: string): Todo {
 
 function read(): Array<Todo> {
   const dbstring = fs.readFileSync(DB_FILE_PATH, "utf-8");
-  const db = JSON.parse( dbstring || "{}" ); /*JSON.parse(dbstring)carrega o arquivo coo string e converte para um banco de dados 
+  const db = JSON.parse(
+    dbstring || "{}"
+  ); /*JSON.parse(dbstring)carrega o arquivo coo string e converte para um banco de dados 
                                                                     que pode ter qualquer tipo de dado. OUTRA COISA, esse || "{}" vai                                                                   servir para que, caso o banco esteja vazio, não retorne erro,                                                                   e sim algo vazio de fato */
-  if (!db.todos) {   //Fail fast validations ou early return
+  if (!db.todos) {
+    //Fail fast validations ou early return
     return [];
   }
 
   return db.todos;
 }
 
-function update(id: string, partialTodo: Partial<Todo>): Todo {
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
   let updatedTodo;
   const todos = read();
   todos.forEach((currentTodo) => {
@@ -90,24 +93,50 @@ function update(id: string, partialTodo: Partial<Todo>): Todo {
   console.log("TODOS ATUALIZADAS", todos);
 }
 
-function updateContentByID(id: string, content: string): Todo {
+function updateContentByID(id: UUID, content: string): Todo {
   return update(id, {
     content,
   });
 }
+
+function deleteById(id: UUID) {
+  const todos = read();
+
+  const todosWithoutOne = todos.filter((todo) => {
+    if (id === todo.id) {
+      return false;
+    }
+    return true;
+  });
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos: todosWithoutOne,
+      },
+      null,
+      2
+    )
+  );
+}
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, ""); //antes de começar a simulação ele vai limpar o banco de dados
 }
 
 //simulação
 CLEAR_DB();
-create("first ToDo ");
-create("segunda ToDo ");
+create("Primeira ToDo ");
+const secondTodo = create("Segunda ToDo ");
+deleteById(secondTodo.id);
+const thirdTodo = create("Terceira ToDo ");
 
-const terceiraTodo = create("Second ToDo");
-//update(terceiraTodo.id, {
+//const terceiraTodo = create("Second ToDo");
+//update(thirdTodo.id, {
 // content: "Atualizada!",
 //  done: true,
 //});
-updateContentByID(terceiraTodo.id, "Atualizada")
-console.log(read());
+updateContentByID(thirdTodo.id, "Atualizada");
+const todos = read();
+console.log(todos);
+console.log(todos.length);
